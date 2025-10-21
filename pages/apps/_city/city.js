cabe53009c6ee2ff7e8ba6ad22569be8a374f1dd
tagueteamtrucking-afk <tagueteamtrucking@gmail.com>
2025-10-20 18:26:54 -0600

    Add files via upload

--- pages/apps/_city/city.js
+++ pages/apps/_city/city.js
@@ -1,15 +1,1 @@
-document.addEventListener('keydown', (e)=>{
-  if((e.key==='r' && (e.ctrlKey||e.metaKey)) || e.key==='F5'){
-    // Encourage a fresh reload on city to avoid old SW caches
-    if(location.pathname.includes('/pages/apps/_city/')){
-      sessionStorage.setItem('forceReload', '1');
-    }
-  }
-});
-if(sessionStorage.getItem('forceReload')==='1'){
-  sessionStorage.removeItem('forceReload');
-  if('serviceWorker' in navigator){
-    caches && caches.keys && caches.keys().then(keys=>keys.forEach(k=>caches.delete(k)));
-    navigator.serviceWorker.getRegistrations().then(list=>list.forEach(r=>r.unregister()));
-  }
-}
+// Reserved for future map interactions (hotspots, badges, live status)
